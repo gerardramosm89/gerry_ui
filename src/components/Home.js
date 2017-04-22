d4c85cd0276383805b7ab0cc986a24dd5876d32d
Cudablogv2 <gerard.ramosm89@gmail.com>
2017-04-22 00:50:28 -0700

    changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 export default class Home extends Component {
   render() {
     return(
-      <div>Home Component</div>
+      <div>
+        Home Component
+        <img src="../../img/avatar3.PNG" />
+        <p>nice the image loads</p>
+      </div>
     );
   }
 }
